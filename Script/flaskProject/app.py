0257a64e3b6ee0ce95729272faad52ab1c864387
zgabi <gabe2.briz@gmail.com>
2023-12-10 14:16:50 +0100

    coomit FIrst GUI

--- Script/flaskProject/app.py
+++ Script/flaskProject/app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from scapy.all import sniff
+
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
